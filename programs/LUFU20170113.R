041a40215130746aeeeec981b67d6b3966e96155
Toshiki Saito <github@jdbase.com>
2017-01-19 07:14:07 +0900

    Refactoring

--- programs/LUFU20170113.R
+++ programs/LUFU20170113.R
@@ -4,12 +4,20 @@
 ##################
 
 YearDif <- function(starting, ending) {
-  as.integer((as.integer(format(as.Date(ending),"%Y%m%d")) - as.integer(format(as.Date(starting),"%Y%m%d")))/10000)
-}  # 満xx年を作る関数
+# 満xx年を作る関数
+  if (starting != "" && ending != "") {
+    period <- as.integer((as.integer(format(as.Date(ending),"%Y%m%d"))
+                          - as.integer(format(as.Date(starting),"%Y%m%d")))/10000)
+  } else {
+    period <- ""
+  }
+  return(period)
+}
 
 FollowupRate <- function(dataframe) {
-  sum(dataframe$followup.in.2y == T) / sum(dataframe$death.before.2y == F)
-}  #follow up率の定義
+# フォローアップ率を作る関数
+    sum(dataframe$followup.in.2y == T) / sum(dataframe$death.before.2y == F)
+}
 
 kFixDateAml05 <- "2016/10/05"
 kFixDateAll02 <- "2016/05/12"
@@ -33,35 +41,35 @@ for (i in 1:length(list$no)) {
 
 # JACLS-ALL-02
 # Pick up data from ALL02
-ALL02pick <- ALL02[, c(2, 6, 45)]  # JACLS登録コード,診断年月日,治療終了日※3
+all02.pick <- ALL02[, c(2, 6, 45)]  # JACLS登録コード,診断年月日,治療終了日※3
 # Pick up data from JACLS
-JACLSpick <- JACLS[, c(11,15, 21, 22, 84)]  # 生年月日,登録コード,生死,死亡日,最終確認日
+jacls.pick <- JACLS[, c(11,15, 21, 22, 84)]  # 生年月日,登録コード,生死,死亡日,最終確認日
 # merge
-merge1 <- merge(ALL02pick, JACLSpick, by.x="JACLS登録コード", by.y="登録コード", all.x=T)
+merge1 <- merge(all02.pick, jacls.pick, by.x="JACLS登録コード", by.y="登録コード", all.x=T)
 names(merge1) <- c("SUBJID", "MHSTDTC", "DATE_END_TRT","BRTHDTC", "DTHFL", "DTHDTC", "DSSTDTC")
 merge1$STUDYID <- "ALL02"  # ALL02のデータセット作成終わり
 
 # JPLSG-AML-05
 # Pick up and proccessing data from AML05(終了日の列)
-AML05pick <- subset(AML05, is.na(AML05$解析対象外))
-AML05pick[is.na(AML05pick)] <- "-"  # Replace NA to "-"
+aml05.pick <- subset(AML05, is.na(AML05$解析対象外))
+aml05.pick[is.na(aml05.pick)] <- "-"  # Replace NA to "-"
 
-for (i in 1:length(AML05pick$J_CD)) {
-  if (AML05pick$中止届有無0なし.1あり[i] == 1) {
-    AML05pick$DATE_END_TRT[i] <- AML05pick$中止届に記載された中止日[i]
-  } else if (AML05pick$移植有無0.なし.1.あり[i] == 1) {
-    AML05pick$DATE_END_TRT[i] <- AML05pick$移植日[i]
+for (i in 1:length(aml05.pick$J_CD)) {
+  if (aml05.pick$中止届有無0なし.1あり[i] == 1) {
+    aml05.pick$DATE_END_TRT[i] <- aml05.pick$中止届に記載された中止日[i]
+  } else if (aml05.pick$移植有無0.なし.1.あり[i] == 1) {
+    aml05.pick$DATE_END_TRT[i] <- aml05.pick$移植日[i]
   } else {
-    AML05pick$DATE_END_TRT[i] <- AML05pick$therapy最終投薬日[i]
+    aml05.pick$DATE_END_TRT[i] <- aml05.pick$therapy最終投薬日[i]
   }
 }
 
-AML05pick1 <- AML05pick[, c(2, 5, 14:16)]
-names(AML05pick1)[4] <- "AML05最終確認日"
+aml05.pick1 <- aml05.pick[, c(2, 5, 14:16)]
+names(aml05.pick1)[4] <- "AML05最終確認日"
 
 # Pick up data from JPLSG
-JPLSGpick <- JPLSG[, c(11,15, 21:23)]
-merge2 <- merge(AML05pick1, JPLSGpick, by.x="J_CD", by.y="登録コード", all.x=T)
+jplsg.pick <- JPLSG[, c(11,15, 21:23)]
+merge2 <- merge(aml05.pick1, jplsg.pick, by.x="J_CD", by.y="登録コード", all.x=T)
 
 # proccessing data from merge data(生死の列)
 merge2$DTHFL <- ifelse(merge2$死亡.0.なし..1.あり == "1", T, merge2$生死)
@@ -116,18 +124,14 @@ ads <- data.set
 # ads <- subset(data.set,data.set$anal.obj == "A")  # 解析対象のみ抽出
 
 #ads$fix.date <- ifelse(ads$STUDYID == "AML05", kFixDateAml05, kFixDateAll02)
-ads$dif.year1 <- YearDif(ads$DSSTDTC, ads$fix.date) #dif.year1には最終確認日-固定日が入る
+ads$y.from.last.update <- YearDif(ads$DSSTDTC, ads$fix.date) #y.from.last.updateにはデータ固定日-最終確認日が入る
 for (i in 1:length(ads$SUBJID)) {
-  if (ads$DTHDTC[i] == "") {
-    ads$dif.year2[i] <- ""
-  } else {
-    ads$dif.year2[i] <- YearDif(ads$DTHDTC[i], ads$fix.date[i])
-  }
-}  # dif.year2には死亡日-データ固定日が入る
-#ads$followup.in.2y <- ifelse(ads$dif.year1 <= 2, T, F)  # 2年以内の転帰確認
-ads$followup.in.2y <- ifelse((is.na(as.numeric(ads$dif.year1)) | as.numeric(ads$dif.year1) > 2),
+  ads$y.from.death[i] <- YearDif(ads$DTHDTC[i], ads$fix.date[i])  # y.from.deathにはデータ固定日-死亡日が入る
+}
+#ads$followup.in.2y <- ifelse(ads$y.from.last.update <= 2, T, F)  # 2年以内の転帰確認
+ads$followup.in.2y <- ifelse((is.na(as.numeric(ads$y.from.last.update)) | as.numeric(ads$y.from.last.update) > 2),
                              F, T)  # 2年以内の転帰確認
-ads$death.before.2y <- ifelse((is.na(as.numeric(ads$dif.year2)) | as.numeric(ads$dif.year2) <=2),
+ads$death.before.2y <- ifelse((is.na(as.numeric(ads$y.from.death)) | as.numeric(ads$y.from.death) <2),
                               F, T)  # 2年時点の死亡確認
 ads$y.end.trt <- YearDif(ads$DATE_END_TRT, ads$fix.date)  # 治療終了後年数
 ads$age.fixed <- YearDif(ads$BRTHDTC, ads$fix.date)  #データ固定時の年齢
@@ -145,7 +149,8 @@ eval(parse(text = paste0("result1 <- data.matrix(rbind(", df.number, "))")))
 
 # setwd("../output")
 # png("Figure1.png", width=800, height=600)
-barplot(result1[,2], ylim=c(0:1), names.arg=c(1:20), main="Follow up rate by years after end of treatment",
+barplot(result1[,2], ylim=c(0:1), names.arg=c(1:20), family="sans",
+        main="Follow up rate by years after end of treatment",
         xlab="Years after end of treatment", ylab="Follow up rate")
 # dev.off()
 
@@ -160,6 +165,7 @@ for (i in 1:35) {
 df.number <- paste("df", c(1:35), sep="", collapse=",")
 eval(parse(text=paste0("result2 <- data.matrix(rbind(", df.number, "))")))
 # png("Figure2.png", width=800, height=600)
-barplot(result2[,2],  ylim=c(0:1), names.arg=c(1:35), main="Follow up rate by age", xlab="Age at data fix", ylab="Follow up rate")
+barplot(result2[,2],  ylim=c(0:1), names.arg=c(1:35), family="sans",
+        main="Follow up rate by age", xlab="Age at data fix", ylab="Follow up rate")
 # dev.off()
 setwd("..")
