70e39e4b87795354073b383beb2e6e75efdb142e
ma3k0 <mamiko.yonejima@nnh.go.jp>
2017-05-16 14:56:41 +0900

    解析対象外集団の背景因子追加

--- programs/background_factors.R
+++ programs/background_factors.R
@@ -13,11 +13,17 @@ dxt.ads$age.diagnosis <- YearDif(dxt.ads$BRTHDTC, dxt.ads$MHSTDTC)
 #ALL02から背景因子用にadsに不足しているデータを取り出し、マージ
 dxt.all02 <- ALL02[, c(2, 8, 10, 29)]
 ds.all02.bf.0 <- merge(dxt.ads, dxt.all02, by.x = "SUBJID", by.y = "JACLS登録コード", all.x = T)
+
 #データ固定2年前以前に死亡した人を除いた集団の作成・計算
-ds.all02.bf <- ds.all02.bf.0[ds.all02.bf.0$no.death.before.2y ==T, ]
+ds.all02.bf <- ds.all02.bf.0[ds.all02.bf.0$no.death.before.2y ==T | ds.all02.bf.0$date.end.trt != "", ]
 age.diagnosis <- summary(ds.all02.bf$age.diagnosis)
 sex <- table(ds.all02.bf$性別)
 marker <- table(ds.all02.bf$マーカー)
 initial.wbc <- summary(ds.all02.bf$WBC..μl.)
+
+#解析対象外集団の作成
+missing.value.bf <- ds.all02.bf.0[ds.all02.bf.0$no.death.before.2y ==F | ds.all02.bf.0$date.end.trt == "", ]
+missing.value.age.diagnosis <- summary(missing.value.bf $age.diagnosis)
+missing.value.sex <- table(missing.value.bf $性別)
 
 setwd("..")
